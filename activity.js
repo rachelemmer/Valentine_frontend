89a4e4e5d4c9477b55711b6fe905ba1d937291d3
github username <github email address>
2020-02-12 12:55:55 -0700

    Update button created

--- activity.js
+++ activity.js
@@ -29,14 +29,28 @@ fetch(`http://localhost:3000/activities/${id}`)
 .then(response => response.json())
 .then(activity => {
     let form = document.createElement('form')
-    form.action = `http://localhost:3000/activites/${id}`
+    form.action = `http://localhost:3000/activities/${id}`
     form.method = 'POST'
     form.innerHTML = `
         <input type='submit' value ="Delete" /> 
         <input type="hidden" name="_method" value="delete" />
         `
-    // document.getElementById('delete_activity').appendChild()
-}) 
+    document.body.appendChild(form)
+})
+
+fetch(`http://localhost:3000/activities/${id}`)
+.then(response => response.json())
+.then(activity => {
+    let form = document.createElement('form')
+    form.action = `http://localhost:3000/activities/${id}`
+    form.method = 'POST'
+    form.innerHTML = `
+        <input type='submit' value ="Update" /> 
+        <input type="hidden" name="_method" value="update" />
+        `
+    document.body.appendChild(form)
+})
+
 
 // li.dataset.id = user.id and li.dataset.name 
 
